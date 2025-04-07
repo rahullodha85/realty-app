ce301bb8f5e3a8fe19d57f9cfa145454d5c86867
rahul <rahul.lodha85@gmail.com>
2025-04-06 23:37:16 -0400

    listing pages and realtor app

--- pages/urls.py
+++ pages/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    # path('contact/', views.contact, name='contact'),
-    # path('services/', views.services, name='services'),
-    # path('blog/', views.blog, name='blog'),
-    # path('blog/<int:post_id>/', views.blog_post, name='blog_post'),
 ]
