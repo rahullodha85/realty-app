163fe61daaad0358778d5e60651977e742a4ccae
rahul <rahul.lodha85@gmail.com>
2025-04-08 10:29:05 -0400

    listings with pagniation

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
-path('search', views.search, name='search'),
+    path('search', views.search, name='search'),
+    path('listings', views.listings_json_resp, name='listings'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from listings.models import Listing
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+
+    page = request.GET.get('page')
+
+    page_listings = paginator.get_page(page)
+
+    context = {
+        'listings': page_listings,
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
 
 def listing(request, listing_id):
     return render(request, 'listings/listing.html', {'listing_id': listing_id})
+
+def listings_json_resp(request):
+    listings = Listing.objects.all()
+
+    listings_json = serializers.serialize('json', listings)
+
+    return HttpResponse(listings_json, content_type='application/json')
