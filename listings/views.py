5c8c73f7664737ea36146aa90a918ca80cb6422f
rahul <rahul.lodha85@gmail.com>
2025-04-09 16:55:57 -0400

    search working

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from listings.choices import bedroom_choices, price_choices, state_choices
 from listings.models import Listing
 
 
@@ -24,7 +25,46 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list,
+        'values': request.GET,
+    }
+    return render(request, 'listings/search.html', context)
 
 def listing(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
--- pages/views.py
+++ pages/views.py
@@ -3,13 +3,17 @@ from lib2to3.fixes.fix_input import context
 from django.shortcuts import render
 
 from listings.models import Listing
+from listings.choices import bedroom_choices, price_choices, state_choices
 from realtors.models import Realtor
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
     }
 
     return render(request, 'pages/index.html', context)
